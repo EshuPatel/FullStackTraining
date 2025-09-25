a5150fdef4476f708f6747fcfba8690c798e3c54
Eshu Patel <138328728+EshuPatel@users.noreply.github.com>
2025-09-25 16:02:38 +0530

    Add files via upload

---
+++ Javascript/sumOfSeries.js
@@ -0,0 +1,9 @@
+var n, i=1, sum=0;
+n=parseInt(prompt("Enter a N value"));
+
+for(i; i<=n; i++){
+    sum=sum+i
+}
+
+document.writeln("<h2> The sum of Series is " + sum+ "<h2>");
+alert("Sum of series is : "+ sum);
